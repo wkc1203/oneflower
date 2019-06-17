9cc7879807970744cdeaaed8acc7f204960f2147
wkc1203@163.com <wkc1203@163.com>
2019-06-17 18:14:11 +0800

    优化

--- js/server.js
+++ js/server.js
@@ -1,8 +1,8 @@
 // const server = 'http://10.0.10.36:8080/
-const server = 'http://10.0.10.79:8085/'
+// const server = 'http://10.0.10.79:8085/'
 // const server = 'http://10.0.11.241:8080/'
 //	
-// const server = 'http://47.100.15.152:8080/'
+const server = 'http://47.100.15.152:8080/'
 	
 const ios_download = 'https://itunes.apple.com/us/app/id1438606770'
 const android_download = server+'uploadFile/app/yizaiyihua.apk'
